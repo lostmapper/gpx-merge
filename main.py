5d7bd2b1ab7da50b1ce124a471974041a622cd82
Brian Kelly <polymonic@gmail.com>
2023-09-02 22:06:14 -0500

    Add pylint and fix issues found by it

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+"""Merge tracks from multiple Strava GPX files into one GPX file"""
 import glob
 import gpxpy.gpx
 
 if __name__ == "__main__":
     gpx_files = glob.glob("./input/*.gpx")
 
-    print(f" {len(gpx_files)} GPX files found")
+    print(f"{len(gpx_files)} GPX files found")
 
     combined_gpx = gpxpy.gpx.GPX()
     combined_gpx.nsmap[
@@ -12,14 +13,14 @@ if __name__ == "__main__":
     ] = "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
 
     for filename in gpx_files:
-        with open(filename, "r") as gpx_file:
+        with open(filename, "r", encoding="utf-8") as gpx_file:
             gpx = gpxpy.parse(gpx_file)
 
             for track in gpx.tracks:
                 print(f"- {track.name}")
                 combined_gpx.tracks.append(track)
 
-    with open("./output/combined.gpx", "w") as combined_file:
+    with open("./output/combined.gpx", "w", encoding="utf-8") as combined_file:
         combined_file.write(combined_gpx.to_xml("1.1"))
 
     print(f"{len(combined_gpx.tracks)} tracks successfully added.")
